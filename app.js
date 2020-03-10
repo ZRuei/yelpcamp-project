76a90e42ad6afe09902061d627a5ced606feef79
ZRuei <bigpawsome@gmail.com>
2020-03-10 12:43:51 +0800

    creating comment to specific campground

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.get('/', (req, res) => {
   res.render('landing');
 });
 
+// ---------------------------------------
+//              CAMPGROUNDS ROUTE
+// ---------------------------------------
 // ----- index: show all campgrounds -----
 app.get('/campgrounds', (req, res) => {
   // get all campgrounds from db
@@ -32,13 +35,15 @@ app.get('/campgrounds', (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render('index', { campgrounds: allCampgrounds });
+      res.render('campgrounds/index', { campgrounds: allCampgrounds });
     }
   });
 });
 
 // ------ create: post new campground -----
 app.post('/campgrounds', (req, res) => {
+  // TODO refactor：修改 new.ejs 的 name 屬性 campground[name] / campground[image]...
+  // 讓 post 直接帶 campground 物件來
   const { name, image, description } = req.body;
   const newCampgroundData = { name, image, description };
   Campground.create(newCampgroundData, (err, newCampground) => {
@@ -53,7 +58,7 @@ app.post('/campgrounds', (req, res) => {
 
 // ----- new: add campground form -----
 app.get('/campgrounds/new', (req, res) => {
-  res.render('new');
+  res.render('campgrounds/new');
 });
 
 // ----- show: specific page -----
@@ -66,11 +71,48 @@ app.get('/campgrounds/:id', (req, res) => {
         console.log(err);
       } else {
         // show the page
-        res.render('show', { campground: foundCampground });
+        res.render('campgrounds/show', { campground: foundCampground });
       }
     });
 });
 
+// -------------------------------------
+//            COMMENTS ROUTE
+// -------------------------------------
+// ----- new: add new comment form -----
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+  // find campground by id
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('comments/new', { campground });
+    }
+  });
+});
+
+// ----- create: post new comment to specific campground page -----
+app.post('/campgrounds/:id/comments', (req, res) => {
+  // find campground by id
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // create new comment
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          // connect new comment to campground
+          foundCampground.comments.push(comment);
+          foundCampground.save();
+          // redirect to campground show page
+          res.redirect(`/campgrounds/${foundCampground._id}`);
+        }
+      });
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log('SERVER CONNECTED!');
