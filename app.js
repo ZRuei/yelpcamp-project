4819a4a91c7e4618651495a0f50c869946415e22
ZRuei <bigpawsome@gmail.com>
2020-03-24 13:30:36 +0800

    fix comment issue

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: t
     console.log('ERROR: ', err.message);
   });
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
