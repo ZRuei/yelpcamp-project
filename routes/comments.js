9c86303fd1a94c187398f3cbd592a82d75f26bc4
ZRuei <bigpawsome@gmail.com>
2020-04-04 12:00:50 +0800

    comment destroy

--- routes/comments.js
+++ routes/comments.js
@@ -12,6 +12,24 @@ function isLoggedIn(req, res, next) {
   res.redirect('/login');
 }
 
+function checkCommentOwnership(req, res, next) {
+  // log in or not?
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if (err) {
+        res.redirect('back');
+      // dose user own the comment?
+      } else if (foundComment.author.id.equals(req.user._id)) {
+        next();
+      } else {
+        res.redirect('back');
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+}
+
 // ----- new: add new comment form -----
 router.get('/new', isLoggedIn, (req, res) => {
   // find campground by id
@@ -53,7 +71,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 // ----- edit comment -----
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
   Comment.findById(req.params.comment_id, (err, comment) => {
     if (err) {
       res.redirect('back');
@@ -64,8 +82,19 @@ router.get('/:comment_id/edit', (req, res) => {
 });
 
 // ----- update comment -----
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
+
+// ----- destroy comment -----
+router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
     if (err) {
       res.redirect('back');
     } else {
